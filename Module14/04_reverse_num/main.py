10e681da894c8e5849603787a1409ecf4a569ec2
Evgeny Petrov <odis743@gmail.com>
2021-12-26 12:20:10 +0300

    solutions to tasks 2 and 4 fixed

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,41 +1,25 @@
-def reverse(number):
-    # TODO Попробуйте все скомпоновать в один цикл и одну функцию, а числа преобразовывать сразу к строкам
-    #  В функции:
-    #  Создаем две переменные - числа до точки, числа после точки, и флаг True\False
-    #  В цикле по строке проверяем условия.
-    #  1) Если текущий символ точка - меняем флаг
-    #  2) Если значение флага True - значит добавляем значение в переменную до точки, сразу добавляем задом наперед
-    #     В другом случае - добавляем значение в переменную после точки также задом наперед
-    #  В завершении цикла делаем сложение строк и возвращаем результат
-    whole_part = number // 1
-    fract_part = number % 1
-    count = 0
 
-    while round(fract_part % 1, 2) > 0:
-        fract_part *= 10
-        count += 1
-    fract_part //= 1
-
-    rev_whole = reverse_number(whole_part)
-    rev_fract_part = reverse_number(fract_part)
-    rev_fract = rev_fract_part / 10 ** count
-    rev_number = rev_whole + rev_fract
-    return(rev_number)
-
-def reverse_number(number):
-    new_number = 0
-    while number != 0:
-        next_num = number % 10
-        number //= 10
-        new_number *= 10
-        new_number += next_num
-    return(new_number)
-
+def reverse(str_number):
+    str1 = str2 = ''
+    flag = False
+    for symbol in (str_number):
+        if symbol == '.':
+           flag = True
+        elif flag:
+            str2 = symbol + str2
+        else:
+            str1 = symbol + str1
+    str_reverse = str1 + '.' + str2
+    num_reverse = float(str_reverse)
+    return num_reverse
 
 N = float(input('Введите число N: '))
+str_N = str(N)
+N_rev = reverse(str_N)
+
 K = float(input('Введите число K: '))
-N_rev = reverse(N)
-K_rev = reverse(K)
+str_K = str(K)
+K_rev = reverse(str_K)
 
 print('\nПервое число наоборот:', N_rev)
 print('Второе число наоборот:', K_rev)
