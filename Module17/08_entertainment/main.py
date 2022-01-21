c4ab6d590ea54df6bdaa5cbeaf91fe73b239ccd8
Evgeny Petrov <odis743@gmail.com>
2022-01-21 19:48:11 +0300

    Домашняя работа к модулю 17

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,23 @@
 # TODO здесь писать код
+import random
+def throw(N, trying, row):
+    L_i = random.randint(0, N - 1)
+    R_i = random.randint(L_i, N - 1)
+    print('Бросок', trying + 1, ': Сбиты палки с номера', L_i + 1, 'по номер', R_i + 1)
+    row_left = row[0: L_i]
+    row_hit = row[L_i: R_i + 1]
+    row_right = row[R_i + 1: N + 1]
+    row_hit = ['.' for _ in row_hit]
+    row_left.extend(row_hit)
+    row_left.extend(row_right)
+    return(row_left)
+
+
+N =random.randint(1, 20)
+# print(N)
+row = ['I' for _ in range(N)]
+K= random.randint(1, 10)
+for trying in range(K):
+    row = throw(N, trying, row)
+    # print('Результат:', row)
+print('\nРезультат:', row)
--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,20 @@
 # TODO здесь писать код
+
+word = input('Введите сообщение: ')
+K = int(input('Введите сдвиг: '))
+
+alphabet = ('абвгдеёжзийклмнопрстуфхцчшщыьъэюя')
+list_alph = list(alphabet)
+
+code_alph = alphabet[K: len(alphabet)]
+code_alph += (alphabet[0: K])
+
+code_word = ''
+for symbol in word:
+    if symbol != ' ':
+        code_sym = code_alph[list_alph.index(symbol)]
+    else:
+        code_sym = ' '
+    code_word += code_sym
+
+print('Зашифрованное сообщение:', code_word)
