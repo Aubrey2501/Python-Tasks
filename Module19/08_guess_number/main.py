a411860d8d7460923c5f9ddc6f2aaa6bd6a816d6
Дмитрий Лоханин <chip1732@mail.ru>
2022-02-10 12:06:22 +0600

    Урок 19. Есть недочеты

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,5 +1,5 @@
-# TODO здесь писать код
 max_number = int(input('Введите максимальное число: '))
+# TODO Стоит загадать число - загаданное_число = randint(1, max_number)
 set_list = {str(i) for i in range(1, max_number + 1)}
 
 question = ''
@@ -9,18 +9,20 @@ while True:
     if question == 'Помогите!':
         break
     else:
+        # TODO Хорошо бы сделать проверки что получили числа set([x for x in question.split() if x.isdigit()])
         set_int = set_list.intersection(set(question.split()))
         print('Ответ Артема:', end=' ')
+        # TODO Ответ "нет" если загаданного числа нет в множестве предположений - загаданое_число not in set_int
+        #  В этом случае делаем объединение двух множеств "exist_set" и "set_int"
+        #  Есть метод "union" https://pythonz.net/references/named/sets.union/
+        #  .
+        #  Соответственно "если загаданное_число in set_int"  ответ да. В этом случае ничего не делаем
         if set_int == set():
             print('Нет')
         else:
             print('Да')
             exist_set.update(set_int)
-
+# TODO Думаю это лучше сделать в условии на 9 строчке
 print('Артём мог загадать следующие числа:', end=' ')
 for symbol in sorted(set_list.difference(exist_set)):
     print(symbol, end=' ')
-
-
-
-
--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,11 +1,16 @@
 # Введите количество человек: 9
-Romanov_list = ['Alexei Peter_I', 'Anna Peter_I', 'Elizabeth Peter_I', 'Peter_II Alexei', 'Peter_III Anna', 'Paul_I Peter_III', 'Alexander_I Paul_I', 'Nicholaus_I Paul_I']
+Romanov_list = ['Alexei Peter_I', 'Anna Peter_I', 'Elizabeth Peter_I', 'Peter_II Alexei', 'Peter_III Anna',
+                'Paul_I Peter_III', 'Alexander_I Paul_I', 'Nicholaus_I Paul_I']
 
 Romanov = dict()
 generations = dict()
-
+# TODO Думаю это лишнее
 Romanov = {succession.split()[0]: succession.split()[1] for succession in Romanov_list}
-
+# TODO Здесь сразу идем циклом с "enumerate" по списку - for _, pair in enumerate(romanov_list)
+#  Разбиваем "pair" на переменные - имя_ребенка, имя_родителя = pair.split()
+#  Делаем проверки, и добавляем значения в один словарь
+#  Если имя_родителя не в словаре - добавляем ключ с нулевым значением
+#  Если имя_ребенка не в словаре - добавляем ключ с ребенком и увеличиваем значение обращаясь к родителю
 for heir in Romanov.keys():
     generation = 1
     child = heir
