1ede8e5546f4f190b1f86a5eab04f9a0497e3101
Evgeny Petrov <odis743@gmail.com>
2022-03-01 19:44:18 +0300

    Урок 21. Домашнее задание

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def fibonacci(num, prev_num, number_it, fbn_lst):
+    new_num = prev_num + num
+    number_it -= 1
+    if number_it >= 0:
+        fbn_lst.append(num)
+        fibonacci(new_num, num, number_it, fbn_lst)
+
+
+position = int(input('Введите позицию числа в ряде Фибоначчи: '))
+fbn_lst = []
+fibonacci(1, 0, position, fbn_lst)
+print('Число: ', fbn_lst[position - 1])
+
+
