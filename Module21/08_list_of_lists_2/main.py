da4e9cff7291a84687042517b703fa22e48b931f
Evgeny Petrov <odis743@gmail.com>
2022-03-04 19:57:13 +0300

    Урок 21. Домашнее задание. Убрал лишнее.

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,16 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+def new_list(lst, new_lst):
+
+    for item in lst:
+        if isinstance(item, int):
+            new_lst.append(item)
+        else:
+            new_list(list(item), new_lst)
+
+    return new_lst
+
+new_lst = []
+print('Ответ:', new_list(nice_list, new_lst))
+
