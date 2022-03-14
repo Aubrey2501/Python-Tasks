4936aa0d30179cc37dad5957aa1ab0246291b566
Evgeny Petrov <odis743@gmail.com>
2022-03-14 22:16:16 +0300

    Практические задания к модулю 22

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+zen_file = open('zen.txt', 'r')
+zen_lst = zen_file.read().split('\n')
+zen_file.close()
+outp_lst = [zen_lst[i] for i in range(len(zen_lst) - 1, -1, -1)]
+print()
+for i_elem in outp_lst:
+    print(i_elem)
