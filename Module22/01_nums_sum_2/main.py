6ab55c2cdfd58e56dae11677c24e4cab68ddf84d
Evgeny Petrov <odis743@gmail.com>
2022-03-14 22:07:02 +0300

    Практические задания к модулю 22

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+input_file = open('numbers.txt', 'r')
+data = input_file.read().split()
+sum_num = sum(int(i) for i in data)
+outp_file = open('answer.txt', 'w')
+outp_file.write(str(sum_num))
+input_file.close()
+outp_file.close()
