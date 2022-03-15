7d9ae19f0211d2512e4f21896ee8a653fab57fc8
Evgeny Petrov <odis743@gmail.com>
2022-03-15 18:03:57 +0300

    Практические задания к модулю 22

--- Module22/09_war_and_peace/main.py
+++ Module22/09_war_and_peace/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+import zipfile
+archive = 'voyna-i-mir.zip'
+with zipfile.ZipFile(archive, 'r') as zip_file:
+    zip_file.extractall('txt_file')
+zip_file.close()
+
+txt_file = open('txt_file/voyna-i-mir.txt', 'r', encoding='utf-8')
+book = txt_file.read()
+txt_file.close()
+
+book_lst = book.split()
+book_lst_txt = ''.join(book_lst)
+
+sym_dict = dict()
+
+for i_sym in book_lst_txt:
+    if i_sym.isalpha() and i_sym not in sym_dict:
+        sym_dict[i_sym] = book_lst_txt.count(i_sym)
+
+sort_dict = sorted(sym_dict.items(), key=lambda x: x[1], reverse=True)
+for i, i_elem in sort_dict:
+    print(i, ':', i_elem)
