b44b58eab6b1dea730f6e30c4972accd156ccd73
Evgeny Petrov <odis743@gmail.com>
2022-03-24 14:15:29 +0300

    Практические задания к модулю 23 (исправлено)

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,40 +1,32 @@
-def ariphm_oper(string):
+def ar_oper(string):
     # TODO Все проверки невалидных данных делайте здесь, и кидайте исключение в основной цикл.
     #  Запись в файл перенесите в основной цикл
-    result = eval(string)
-    out_file = open('result.txt', 'a')
-    print(result)
-    out_file.write(str(result) + '\n')
-
+    string = string.replace('\n', '')
+    res_lst = string.split(' ')
+    if not res_lst[0].isdigit() or not res_lst[2].isdigit():
+        raise ValueError
+    elif res_lst[1] not in arithm_oper:
+        raise ValueError
+    else:
+        result = eval(string)
+        return f'{res_lst[0]} {res_lst[1]} {res_lst[2]} = {result}'
 
 arithm_oper = ['+', '-', '*', '/', '//', '%']
 # TODO Открывайте файлы через контекстный менеджер
-inp_file = open('calc.txt', 'r')
-out_file = open('result.txt', 'w')
-for i_line in inp_file:
-    try:
-        line_lst = i_line.split()
-        flag_err = False
-        if not line_lst[0].isdigit() or not line_lst[2].isdigit():
-            flag_err = True
-            raise ValueError
-        elif line_lst[1] not in arithm_oper:
-            flag_err = True
-            raise ValueError
-        else:
-            print(i_line, end=' = ')
-            ariphm_oper(i_line)
+with open('calc.txt', 'r') as inp_file, open('result.txt', 'w') as out_file:
+    for i_line in inp_file:
+        try:
+            result = ar_oper(i_line)
 
+        except ValueError:
+            print('Ошибка в строке', i_line)
+            answer = input('Хотите исправить (Да/Нет)?: ').lower()
+            if answer == 'да':
+                i_line = input('Введите исправленную строку: ')
+                result = ar_oper(i_line)
 
-    except ValueError:
-        print('Ошибка в строке', i_line)
-        answer = input('Хотите исправить (Да/Нет)?: ').lower()
-        if answer == 'да':
-            i_line = input('Введите исправленную строку: ')
-            print(i_line, end=' = ')
-            ariphm_oper(i_line)
+        print(result)
+        out_file.write(str(result) + '\n')
 
-        else:
-            continue
-
+inp_file.close()
 out_file.close()
