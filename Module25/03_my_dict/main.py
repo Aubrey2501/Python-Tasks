d6239ecf6617bc0242c6804a600d8db9b57877c7
Evgeny Petrov <odis743@gmail.com>
2022-04-08 16:13:57 +0300

    Практические задания к модулю 25

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,8 +1,8 @@
-class Dict:
+class MyDict:
 
     def __init__(self, dct):
         self.dct = dct
-        self.dct_value = ''
+        self.dct_value = 0
 
     def set_dct(self, dct_key):
         if dct_key.isdigit():
@@ -10,12 +10,13 @@ class Dict:
         if dct_key in self.dct:
             self.dct_value = self.dct[dct_key]
 
-    def get_dct(self):
+    def get(self, dct_key):
+        self.set_dct(dct_key)
         return self.dct_value
 
 
 example_dct = {0: 'Вася', 1: 'Петя', 3: 'Саша', 4: 'Вова'}
-my_dict = Dict(example_dct)
-i_key = input('Введите ключ словаря: ')
-my_dict.set_dct(i_key)
-print(my_dict.get_dct())
+while True:
+    my_dict = MyDict(example_dct)
+    i_key = input('Введите ключ словаря: ')
+    print(my_dict.get(i_key))
