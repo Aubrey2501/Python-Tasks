247fc387fb4702ce72752dd9682d006bd8a28449
Evgeny Petrov <odis743@gmail.com>
2022-04-08 14:56:52 +0300

    Практические задания к модулю 25

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,1 +1,43 @@
-# TODO здесь писать код
+import random
+
+
+class Karma:
+    __exceptions = ['KillError', 'DrunkError', 'CarCrashError', 'GluttonyError', 'DepressionError']
+    __count = 0
+
+    def __init__(self):
+        self.__karma = 0
+        self.__day_karma = 0
+
+    def set_carma(self, rand_karma):
+        self.__count += 1
+        self.__day_karma = random.choices((rand_karma, random.choice(self.__exceptions)), weights=[10, 1])[0]
+        if self.__day_karma not in self.__exceptions:
+            self.__karma += self.__day_karma
+        else:
+            raise Exception(self.__day_karma)
+
+    def get_karma(self):
+        return self.__karma
+
+    def get_count(self):
+        return self.__count
+
+
+def one_day(karma):
+    rand_karma = random.randint(1, 7)
+    karma.set_carma(rand_karma)
+
+
+my_karma = Karma()
+file = open('karma.log', 'w')
+while True:
+    try:
+        one_day(my_karma)
+        if my_karma.get_karma() >= 5000:
+            print('\nIt took {} days to reach 5000 karma points'.format(my_karma.get_count()))
+            file.close()
+            break
+
+    except Exception as error:
+        file.write('Day {}: Exception {}\n'.format(my_karma.get_count(), error))
--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+class Dict:
+
+    def __init__(self, dct):
+        self.dct = dct
+        self.dct_value = ''
+
+    def set_dct(self, dct_key):
+        if dct_key.isdigit():
+            dct_key = int(dct_key)
+        if dct_key in self.dct:
+            self.dct_value = self.dct[dct_key]
+
+    def get_dct(self):
+        return self.dct_value
+
+
+example_dct = {0: 'Вася', 1: 'Петя', 3: 'Саша', 4: 'Вова'}
+my_dict = Dict(example_dct)
+i_key = input('Введите ключ словаря: ')
+my_dict.set_dct(i_key)
+print(my_dict.get_dct())
