f5549ed0fb898e83419dd6ffe5767ea6b7a07122
Evgeny Petrov <odis743@gmail.com>
2022-04-09 16:32:54 +0300

    Практические задания к модулю 25

--- Module25/05_auto/main.py
+++ Module25/05_auto/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+# coding: utf-8
+import math
+import random
+
+
+class Car:
+    def __init__(self, x, y, dir):
+        self.coord_x = x
+        self.coord_y = y
+        self.direction = dir
+
+    def turn(self, angle):
+        if angle <= 180:
+            self.direction += angle
+        else:
+            self.direction -= (360 - angle)
+        if self.direction >= 360:
+            self.direction -= 360
+        print('Текущее направление: ', self.direction, 'градусов')
+
+    def move(self, distance):
+        self.coord_x += distance * math.cos(self.direction)
+        self.coord_x = round(self.coord_x, 1)
+        self.coord_y += distance * math.sin(self.direction)
+        self.coord_y = round(self.coord_y, 1)
+
+    def get_coord(self):
+        return 'Текущие координаты: {} : {}\n'.format(self.coord_x, self.coord_y)
+
+
+my_car = Car(0, 0, 15)
+my_car.turn(25)
+my_car.move(30)
+print(my_car.get_coord())
