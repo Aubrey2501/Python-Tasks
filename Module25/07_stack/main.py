423bc45169c71a4a5a976cb555e672603d8ecda6
Evgeny Petrov <odis743@gmail.com>
2022-04-10 22:01:06 +0300

    Практические задания к модулю 25

--- Module25/07_stack/main.py
+++ Module25/07_stack/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+class Steck:
+    def __init__(self):
+        self.stack = dict()
+
+    def add(self, key, value):
+        self.stack[key] = value
+
+    def remove(self, other):
+        element = self.stack.pop()
+        return element
+
+    def __str__(self):
+        return self.stack
+
+class TaskManager:
+    def __init__(self, task: str, priority: int):
+        self.steck = my_steck
+        self.task = task
+        self.priority = priority
+
+    def new_task(self, task, priority):
+        self.steck[task] = priority
+
+
+
+my_steck = Steck()
