85ae0046e96a7c8384c96380b101b5569f3dc9ee
Evgeny Petrov <odis743@gmail.com>
2022-04-10 20:25:28 +0300

    Практические задания к модулю 25

--- Module25/06_cohabitation_2/main.py
+++ Module25/06_cohabitation_2/main.py
@@ -11,9 +11,11 @@ class House:
         self.dirt += 5
 
     def __str__(self):
+        self.get_dirty()
         return 'Дом: Еда: {}, Деньги: {}, Кошачий корм: {}, Степень загрязнения: {}'.format(
             self.food, self.money, self.cat_food, self.dirt
         )
+
 
 class Man:
     def __init__(self):
@@ -21,6 +23,22 @@ class Man:
         self.satiety = 30
         self.happy = 100
         self.alive = True
+        self.house = my_house
+
+    def eat(self):
+        if self.house.food >= 30:
+            food_amount = random.randint(1, 30)
+        elif self.house.food > 1:
+            food_amount = random.randint(1, self.house.food // 2)
+        else:
+            food_amount = self.house.food
+        self.satiety += food_amount
+        self.house.food -= food_amount
+        return 'ест {} пищи'.format(food_amount)
+
+    def pet_the_cat(self):
+        self.happy += 5
+        self.satiety -= 10
 
     def __str__(self):
         return 'сытость: {}, удовлетворенность: {}'.format(self.satiety, self.happy)
@@ -49,15 +67,13 @@ class Husband(Man):
             if self.house.dirt > 90:
                 self.happy -= 10
             if self.satiety <= 10 and self.house.food > 0:
-                if self.house.food >= 30:
-                    food_amount = random.randint(1, 30)
-                else:
-                    food_amount = random.randint(1, self.house.food)
-                self.eat(food_amount)
-            elif self.house.money <= 50 and self.satiety >= 1:
+                self.eat()
+            elif self.house.money < 100 and self.satiety >= 1:
                 self.work()
-            else:
+            elif self.happy <= 10:
                 self.pet_the_cat()
+            else:
+                self.play()
 
     def is_alive(self):
         info = super().is_alive(self.satiety, self.happy)
@@ -66,11 +82,9 @@ class Husband(Man):
             print(info)
         return self.alive
 
-
-    def eat(self, amount):
-        print('{} ест'.format(self.name))
-        self.satiety += amount
-        self.house.food -= amount
+    def eat(self):
+        info = super().eat()
+        print(self.name, info)
 
     def work(self):
         print('{} идет на работу'.format(self.name))
@@ -83,9 +97,9 @@ class Husband(Man):
         self.satiety -= 10
 
     def pet_the_cat(self):
+        super().pet_the_cat()
         print('{} гладит кота'.format(self.name))
-        self.happy += 5
-        self.satiety -= 10
+
 
     def __str__(self):
         info = super().__str__()
@@ -112,31 +126,26 @@ class Wife(Man):
             if self.house.dirt > 90:
                 self.happy -= 10
             if self.satiety <= 10 and self.house.food > 0:
-                if self.house.food >= 30:
-                    food_amount = random.randint(1, 30)
-                else:
-                    food_amount = random.randint(1, self.house.food)
-                self.eat(food_amount)
-            elif (self.house.food <= 15 or self.house.cat_food <= 5) and self.house.money >= 15:
+                self.eat()
+            elif (self.house.food <= 30 or self.house.cat_food <= 10) and self.house.money >= 40:
                 self.buy_food()
-            elif self.happy <= 15 and self.house.money >= 350:
-                self.shopping()
-            elif self.house.dirt >= 10 and self.satiety >= 1:
+            elif self.house.dirt >= 50 and self.satiety >= 1:
                 self.clean()
+            elif self.happy <= 10 and self.house.money >= 350:
+                self.shopping()
             else:
                 self.pet_the_cat()
             self.__str__()
 
-    def eat(self, amount):
-        print('{} ест'.format(self.name))
-        self.satiety += amount
-        self.house.food -= amount
+    def eat(self):
+        info = super().eat()
+        print(self.name, info)
 
     def buy_food(self):
         print('{} покупает продукты'.format(self.name))
-        self.house.food += 10
-        self.house.cat_food += 5
-        self.house.money -= 15
+        self.house.food += 30
+        self.house.cat_food += 10
+        self.house.money -= 40
         self.satiety -= 10
 
     def shopping(self):
@@ -155,9 +164,8 @@ class Wife(Man):
         self.satiety -= 10
 
     def pet_the_cat(self):
+        super().pet_the_cat()
         print('{} гладит кота'.format(self.name))
-        self.happy += 5
-        self.satiety -= 10
 
     def __str__(self):
         info = super().__str__()
@@ -189,12 +197,8 @@ class Cat:
         if not self.alive:
             raise Exception('Кот {} мертв!'.format(self.name))
         else:
-            if self.satiety <= 10 and self.house.cat_food > 0:
-                if self.house.food >= 10:
-                    food_amount = random.randint(1, 10)
-                else:
-                    food_amount = random.randint(1, self.house.food)
-                self.eat(food_amount)
+            if self.satiety <= 15 and self.house.cat_food > 0:
+                self.eat()
             else:
                 rand_action = random.choice(['wallpaper', 'sleep'])
                 if rand_action == 'wallpaper':
@@ -203,13 +207,19 @@ class Cat:
                     self.sleep()
 
     def __str__(self):
-        return 'Кот {}, Сытость: {}'.format(self.name, self.satiety)
+        return 'Кот: {}, Сытость: {}'.format(self.name, self.satiety)
 
 
-    def eat(self, amount):
-        print('Кот {} ест'.format(self.name))
-        self.satiety += amount
-        self.house.cat_food -= amount
+    def eat(self):
+        if self.house.cat_food >= 10:
+            food_amount = random.randint(1, 10)
+        elif self.house.cat_food > 1:
+            food_amount = random.randint(1, self.house.cat_food)
+        else:
+            food_amount = self.house.cat_food
+        print('Кот {} ест {} кошачьего корма'.format(self.name, food_amount))
+        self.satiety += food_amount
+        self.house.cat_food -= food_amount
 
     def sleep(self):
         print('Кот {} спит'.format(self.name))
@@ -225,7 +235,6 @@ class Cat:
             self.alive = False
             print('Кот {} умер от голода!'.format(self.name))
         return self.alive
-
 
 
 my_house = House()
