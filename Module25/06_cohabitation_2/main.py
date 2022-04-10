3234bbfeb0c1d41660d2dc8debcefdc569d3f2df
Evgeny Petrov <odis743@gmail.com>
2022-04-10 13:48:12 +0300

    Практические задания к модулю 25

--- Module25/06_cohabitation_2/main.py
+++ Module25/06_cohabitation_2/main.py
@@ -11,7 +11,7 @@ class House:
         self.dirt += 5
 
     def __str__(self):
-        return 'Дом:\n Еда: {}, Деньги: {}, Кошачий корм: {}, Степень загрязнения: {}'.format(
+        return 'Дом: Еда: {}, Деньги: {}, Кошачий корм: {}, Степень загрязнения: {}'.format(
             self.food, self.money, self.cat_food, self.dirt
         )
 
@@ -42,13 +42,13 @@ class Husband(Man):
         self.house = house
 
     def action(self):
-        self.is_alive()
+        self.alive = self.is_alive()
         if not self.alive:
             raise Exception('{} мертв!'.format(self.name))
         else:
             if self.house.dirt > 90:
                 self.happy -= 10
-            if self.satiety <= 5 and self.house.food > 0:
+            if self.satiety <= 10 and self.house.food > 0:
                 if self.house.food >= 30:
                     food_amount = random.randint(1, 30)
                 else:
@@ -64,20 +64,26 @@ class Husband(Man):
         if not self.alive:
             info = ' '.join((self.name, info))
             print(info)
+        return self.alive
+
 
     def eat(self, amount):
+        print('{} ест'.format(self.name))
         self.satiety += amount
         self.house.food -= amount
 
     def work(self):
+        print('{} идет на работу'.format(self.name))
         self.house.money += 150
         self.satiety -= 10
 
     def play(self):
+        print('{} играет на компьютере'.format(self.name))
         self.happy += 20
         self.satiety -= 10
 
     def pet_the_cat(self):
+        print('{} гладит кота'.format(self.name))
         self.happy += 5
         self.satiety -= 10
 
@@ -88,6 +94,7 @@ class Husband(Man):
                 'Имя: {}, Состояние:'.format(self.name),
                 info)
         )
+        return info
 
 
 class Wife(Man):
@@ -98,12 +105,13 @@ class Wife(Man):
         self.fur = 0
 
     def action(self):
+        self.alive = self.is_alive()
         if not self.alive:
             raise Exception('{} мертв!'.format(self.name))
         else:
             if self.house.dirt > 90:
                 self.happy -= 10
-            if self.satiety <= 5 and self.house.food > 0:
+            if self.satiety <= 10 and self.house.food > 0:
                 if self.house.food >= 30:
                     food_amount = random.randint(1, 30)
                 else:
@@ -120,29 +128,34 @@ class Wife(Man):
             self.__str__()
 
     def eat(self, amount):
+        print('{} ест'.format(self.name))
         self.satiety += amount
         self.house.food -= amount
 
     def buy_food(self):
+        print('{} покупает продукты'.format(self.name))
         self.house.food += 10
         self.house.cat_food += 5
         self.house.money -= 15
         self.satiety -= 10
 
     def shopping(self):
+        print('{} идет в магазин за шубой'.format(self.name))
         self.happy += 60
         self.fur += 1
         self.house.money -= 350
         self.satiety -= 10
 
     def clean(self):
-        if self.house.dirt <= 100
+        print('{} убирает дом'.format(self.name))
+        if self.house.dirt <= 100:
             self.house.dirt = 0
         else:
             self.house.dirt -= 100
         self.satiety -= 10
 
     def pet_the_cat(self):
+        print('{} гладит кота'.format(self.name))
         self.happy += 5
         self.satiety -= 10
 
@@ -153,12 +166,14 @@ class Wife(Man):
                 'Имя: {}, Состояние:'.format(self.name),
                 info)
         )
+        return info
 
     def is_alive(self):
         info = super().is_alive(self.satiety, self.happy)
         if not self.alive:
             info = ' '.join((self.name, info))
             print(info)
+        return self.alive
 
 
 class Cat:
@@ -170,10 +185,11 @@ class Cat:
         self.alive = True
 
     def action(self):
+        self.alive = self.is_alive()
         if not self.alive:
             raise Exception('Кот {} мертв!'.format(self.name))
         else:
-            if self.satiety <= 3 and self.house.cat_food > 0:
+            if self.satiety <= 10 and self.house.cat_food > 0:
                 if self.house.food >= 10:
                     food_amount = random.randint(1, 10)
                 else:
@@ -191,13 +207,44 @@ class Cat:
 
 
     def eat(self, amount):
+        print('Кот {} ест'.format(self.name))
         self.satiety += amount
         self.house.cat_food -= amount
 
     def sleep(self):
+        print('Кот {} спит'.format(self.name))
         self.satiety -= 10
 
     def tearing_wallpaper(self):
+        print('Кот {} дерёт обои'.format(self.name))
         self.house.dirt += 5
         self.satiety -= 10
 
+    def is_alive(self):
+        if self.satiety <= 0:
+            self.alive = False
+            print('Кот {} умер от голода!'.format(self.name))
+        return self.alive
+
+
+
+my_house = House()
+husband = Husband('Виталик', my_house)
+wife = Wife('Матильда', my_house)
+cat = Cat('Барсик', my_house)
+
+for i_day in range(366):
+    try:
+        print('\nДень:', i_day + 1)
+        if husband.is_alive():
+            husband.action()
+        if wife.is_alive():
+            wife.action()
+        if cat.is_alive():
+            cat.action()
+        print(my_house)
+        print(husband)
+        print(wife)
+        print(cat)
+    except Exception as error:
+        print(error)
