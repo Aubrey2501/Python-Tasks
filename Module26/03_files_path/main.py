fc4144561b8d7d16538c8e278ae327bd764b0c0d
Evgeny Petrov <odis743@gmail.com>
2022-04-23 13:11:47 +0300

    Изменил функцию find_dir, превратив ее в генератор

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -10,9 +10,9 @@ def find_dir(dir_name):
     Returns:
         root(str): абсолютный путь к искомому каталогу
     """
-    for root, dirs, files in os.walk(top='c:/'):
-        if os.path.isdir(root) and root.endswith(dir_name):
-            return root
+    dir_lst = (i_root for i_root, i_dirs, i_files in os.walk(top='c:/')
+               if os.path.isdir(i_root) and i_root.endswith(dir_name))
+    yield dir_lst
 
 
 def gen_files_path(dir_name):
@@ -23,20 +23,24 @@ def gen_files_path(dir_name):
     Returns:
         files_list (generator list): список- генератор с именами файлов
     """
-    dir_path = find_dir(dir_name)
-    dir_list = os.listdir(dir_path)
+    dir_list = os.listdir(dir_name)
     for i_elem in dir_list:
-        i_path = os.path.join(dir_path, i_elem)
+        i_path = os.path.join(dir_name, i_elem)
         if os.path.isdir(i_path):
             files_list = (os.path.join(i_path, i_file) for i_file in os.listdir(i_path) if os.path.isdir(i_path))
             yield files_list
         else:
             print(i_path)
 
-print('\n')
-files_list = gen_files_path(dir_name='Module26')
-for item in files_list:
-    for i_elem in item:
-        print(i_elem)
+
+print()
+dir_list = find_dir(dir_name='Module26')
+for i_elem in dir_list:
+    dir_list.close()
+    for i_dir in i_elem:
+        files_list = gen_files_path(dir_name=str(i_dir))
+        for item in files_list:
+            for i_file in item:
+                print(i_file)
 
 
