8dd849686f72c312f7ee6bbf403dc437f063b551
Evgeny Petrov <odis743@gmail.com>
2022-04-27 19:38:04 +0300

    Практические задания к модулю 27

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+from typing import Callable, Any
+
+COUNTER = dict()
+
+
+def counter(func: Callable) -> Callable:
+    """Декоратор. Считает и выводит количество вызовов функции"""
+    def wrapped_func(*args: Any, **kwargs: Any) -> Any:
+        if func.__name__ not in COUNTER:
+            COUNTER[func.__name__] = 1
+        else:
+            COUNTER[func.__name__] += 1
+        print(f'Функция {func.__name__}. Вызовов: {COUNTER[func.__name__]}')
+        result = func(*args, **kwargs)
+        return result
+    return wrapped_func
+
+
+@counter
+def test():
+    print('<Тут что-то происходит...>')
+
+
+test()
+test()
+test()
+
