9b629f848ac3c10bd7c62dc1e4b7c30f3df18beb
Evgeny Petrov <odis743@gmail.com>
2022-04-27 12:22:58 +0300

    Практические задания к модулю 27

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+from typing import Callable, Any
+
+
+def how_are_you(func: Callable) -> Any:
+    """Декоратор, задающий вопрос и выводящий ответ"""
+    def wrapped_func(*args: Any, **kwargs: Any) -> Callable:
+        question = input('Как дела? ')
+        print('А у меня не очень')
+        result = func(*args, **kwargs)
+        return result
+    return wrapped_func
+
+@how_are_you
+def test():
+    print('<Тут что-то происходит...>')
+
+
+test()
--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+from typing import Callable, Any, Tuple
+import time
+
+
+def slower(func: Callable) -> Any:
+    def wrapped_func(*args: Any, **kwargs: Any) -> Any:
+        time.sleep(2)
+        result = func(*args, **kwargs)
+        return result
+    return wrapped_func
+
+
+@slower
+def sqrt_gen(num: int) -> str:
+    """Функция, возвращающая квадрат и куб числа"""
+    sqrt_num = num ** 2
+    cube_num = num ** 3
+    result = f'{num} ** 2 = {sqrt_num:>2};  {num} ** 3 = {cube_num}'
+    return result
+
+
+for i_num in range(1, 11):
+    print(sqrt_gen(i_num))
+
+
