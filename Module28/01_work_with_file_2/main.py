6920b321a04bf09a4497138a780075327c8b384d
Evgeny Petrov <odis743@gmail.com>
2022-05-03 14:53:00 +0300

    Практические задания к модулю 28

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+class File:
+    def __init__(self, file_name: str, mode: str) -> None:
+        self.__file_name = file_name
+        self.__mode = mode
+
+    def __enter__(self):
+        try:
+            self.__file = open(self.__file_name, self.__mode, encoding='utf-8')
+        except FileNotFoundError:
+            self.__file = open(self.__file_name, 'w')
+            self.__file = open(self.__file_name, self.__mode, encoding='utf-8')
+        return self.__file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is FileNotFoundError or exc_type is FileExistsError:
+            return True
+        self.__file.close()
+
+
+with File('example.txt', 'r') as file:
+    text = file.read().split()
+with File('example.txt', 'w') as file:
+    file.write('Всем привет!')
