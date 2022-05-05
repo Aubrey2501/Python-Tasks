fc193a4d8bbad39b4fc947eab17fe5f1a45ab9fd
Evgeny Petrov <odis743@gmail.com>
2022-05-05 15:19:21 +0300

    Практические задания к модулю 28

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -29,6 +29,8 @@ class MyMath(ABC):
 res_1 = MyMath.circle_len(radius=5)
 res_2 = MyMath.circle_sq(radius=6)
 res_3 = MyMath.cube_volume(length=12.5)
+res_4 = MyMath.sphere_area(radius=5.5)
 print(res_1)
 print(res_2)
 print(res_3)
+print(res_4)
