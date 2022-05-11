fc039a223a6eab7f03c019e610eee87475a9776a
Evgeny Petrov <odis743@gmail.com>
2022-05-11 15:29:15 +0300

    Практические задания к модулю 29

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+import functools
+from typing import Callable
+from functools import wraps
+
+user_permissions = ['admin']
+
+
+def check_permission(func):
+    @functools.wraps
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print('wrapping')
+        return result
+    return wrapper
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
