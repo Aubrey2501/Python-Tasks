dcd5ec94c49b3b1959f200a4bf6ac43bb0438b7c
Evgeny Petrov <odis743@gmail.com>
2022-05-12 14:51:16 +0300

    Практические задания к модулю 29

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+import functools
+
+
+def callback(path):
+    def application(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            if path == args[0]:
+                return func
+            return False
+        return wrapped
+    return application
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+app = {'//': example('//')}
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
