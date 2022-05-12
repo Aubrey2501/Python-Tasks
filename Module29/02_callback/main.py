985e24d7dd2a4fe094b63d58e4ead14090a2f38a
Evgeny Petrov <odis743@gmail.com>
2022-05-12 15:08:59 +0300

    Практические задания к модулю 29

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,10 +1,14 @@
 import functools
+from typing import Callable
 
 
-def callback(path):
-    def application(func):
+def callback(path: str) -> Callable:
+    """Декоратор - функция обратного вызова
+       Args: path: аргумент для проверки условия запуска передаваемой функции"""
+    def application(func: Callable) -> Callable:
         @functools.wraps(func)
         def wrapped(*args, **kwargs):
+            print(path, args, kwargs)
             if path == args[0]:
                 return func
             return False
