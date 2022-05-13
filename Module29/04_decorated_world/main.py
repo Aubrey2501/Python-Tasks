bb3271037128dec1928f81a46b056f1d86da259e
Evgeny Petrov <odis743@gmail.com>
2022-05-13 17:46:18 +0300

    Практические задания к модулю 29

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+import functools
+from typing import Callable
+
+
+def decorator_with_args_for_any_decorator(decorator: Callable) -> Callable:
+    """Декоратор, передающий в функцию аргументы"""
+    def decorator_maker(*args, **kwargs) -> Callable:
+        def decorator_wrapper(func: Callable) -> Callable:
+            return decorator(func, *args, **kwargs)
+        return decorator_wrapper
+    return decorator_maker
+
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func: Callable, *dec_args, **dec_kwargs) -> Callable:
+    """Декоратор - шаблон"""
+    functools.wraps(func)
+    def wrapper(*func_args, **func_kwargs) -> Callable:
+        print('Переданные арги и кварги в декоратор:', func_args, func_kwargs)
+        return func(*func_args, **func_kwargs)
+    return wrapper
+
+
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет", text, num)
+
+
+decorated_function("Юзер", 101)
