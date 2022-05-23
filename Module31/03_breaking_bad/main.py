6350b3ed11711026066319ab7f49eb3ecc8c1cc4
Evgeny Petrov <odis743@gmail.com>
2022-05-23 20:49:53 +0300

    Практическое задание к модулю 31

--- Module31/03_breaking_bad/main.py
+++ Module31/03_breaking_bad/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+import json
+import functools
+import requests
+
+# api = 'https://www.breakingbadapi.com/api/deaths'
+# my_info = requests.get(api)
+# my_json = json.loads(my_info.text)
+#
+#
+# with open('breakingbad.json', 'w') as file:
+#     json.dump( my_json, file, indent=4)
+
+with open('breakingbad.json', 'r') as file:
+    lst_dict = json.load(file)
+    # print(lst_dict)
+
+sort_lst = sorted(lst_dict, key=lambda x: x.get('season') + x.get('episode'))
+
+
+print(functools.reduce(lambda count, item: count + item.get('number_of_deaths'), sort_lst, 0))
+
+
