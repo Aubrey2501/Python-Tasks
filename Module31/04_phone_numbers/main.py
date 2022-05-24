bc6b8d3920b6e7956f7b428ca2814c017aab476d
Evgeny Petrov <odis743@gmail.com>
2022-05-24 14:58:33 +0300

    Практическое задание к модулю 31

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import re
+
+telephone_lst = ['9999999999', '999999-999', '99999x9999', '8495123456', '84991234567']
+numbers = ("первый", "второй", "третий", "четвертый", "пятый", "шестой", "седьмой", "восьмой", "девятый", "десятый")
+
+for elem in telephone_lst:
+    print('{} номер \t{}:\t'.format(numbers[telephone_lst.index(elem)], elem), end=' ')
+    result = re.match(r'\b[8-9]\d{9}\b', elem)
+    if result:
+        print('все в порядке')
+    else:
+        print('не подходит')
+
