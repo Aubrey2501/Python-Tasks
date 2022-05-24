366c6c89a33b13c854c77c679802e66697916d64
Evgeny Petrov <odis743@gmail.com>
2022-05-24 18:16:54 +0300

    Практическое задание к модулю 31

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import requests
+import json
+
+web_page = 'http://www.columbia.edu/~fdc/sample.html'
+
+info = requests.get(web_page)
+my_json = json.loads(info.text)
+
+with open('test.json', 'w') as file:
+    json.dump(my_json, file, indent=4)
