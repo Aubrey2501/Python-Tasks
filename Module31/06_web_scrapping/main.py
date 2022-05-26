fe1bee2cdfbb429b5886ea8cd9a018cc7d0ee99d
Evgeny Petrov <odis743@gmail.com>
2022-05-26 19:19:55 +0300

    Практическое задание к модулю 31

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,23 +1,14 @@
 import requests
 import re
-from bs4 import BeautifulSoup
 
-# TODO Круто, но задачу можно решить через регулярные выражения.
-#  Попробуйте написать паттерн, который бы выделять только текст из "h3"
-#  То есть есть примерно такая строка - <h3 id="contents">CONTENTS</h3>
-#  Из нее нужно достать только текст "CONTENTS" для этого пригодятся скобочные группы
-#  И получается что-то вроде - <h3 .*?>(скобочная группа) закрывающий тег h3
+
 def get_h4(url):
     info = requests.get(url).content
-    pattern = ['\<h3 id\=\"contents\"\>CONTENTS\<\/h3>']
-    elems = re.findall('^(pattern)')
 
-
-    # soup = BeautifulSoup(info, 'html.parser')
-    # result = []
-    # for link in soup.find_all('h3'):
-    #     result.append(link.get_text())
-    # return result
+    # with open('test.txt', 'r') as file:
+    #     info = file.read()
+    result = re.findall(r'<h3 .*?>(.*?)</h3>', str(info))
+    return result
 
 
 page = 'http://www.columbia.edu/~fdc/sample.html'
