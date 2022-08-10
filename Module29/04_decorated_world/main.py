73dda4b4bbaeca4d350d23b77cd5b543fe797f5e
Evgeny Petrov <odis743@gmail.com>
2022-08-10 14:47:46 +0300

    Практическое задание к модулю 31 (меняем аргументы, передаваемые в фунцию)

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -8,9 +8,7 @@ def decorator_with_args_for_any_decorator(decorator: Callable) -> Callable:
     def decorator_maker(*args, **kwargs) -> Callable:
         def decorator_wrapper(func: Callable) -> Callable:
             return decorator(func, *args, **kwargs)
-
         return decorator_wrapper
-
     return decorator_maker
 
 
@@ -21,17 +19,24 @@ def decorated_decorator(func: Callable, *dec_args, **dec_kwargs) -> Callable:
 
     def wrapper(*func_args, **func_kwargs) -> Callable:
         print('Переданные арги и кварги в декоратор:', func_args, func_kwargs)
-        return func(*func_args, **func_kwargs)
 
+        # меняем параметр, передаваемый в функцию decorated_function
+        func_kwargs1 = dict()
+        for i_key, i_val in func_kwargs.items():
+            if isinstance(i_val, int):
+                i_val += 100
+            func_kwargs1[i_key] = i_val
+
+        return func(*func_args, **func_kwargs1)
     return wrapper
 
 
 @decorated_decorator(100, 'рублей', 200, 'друзей')
-def decorated_function(text: str, num: int) -> None:
+def decorated_function(text=None, num=None) -> None:
     print("Привет", text, num)
 
 
-decorated_function("Юзер", 101)
+decorated_function("Юзер", num=101)
 
 # зачет!
 
